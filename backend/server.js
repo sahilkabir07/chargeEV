f53db2fe8b2b678cd3b6e92dbf550a0ecac48fcc
sahilkabir07 <sahilakthar8986@gmail.com>
2025-06-02 19:15:10 +0530

    server and vercel files modified

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,23 @@ const cors = require("cors");
 const { connectDB } = require("./config");
 const userRoutes = require("./routes/userRoutes");
 const chargerRoutes = require("./routes/chargerRoutes");
-require("dotenv").config();
 const bookingRoutes = require("./routes/bookingRoutes");
 const serverless = require("serverless-http");
+require("dotenv").config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 connectDB();
-app.use("/", (req, res) => {
-  res.send("hii..");
-});
+
 app.use("/api/users", userRoutes);
 app.use("/api/chargers", chargerRoutes);
 app.use("/api/bookings", bookingRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("hii..");
+});
+
 module.exports.handler = serverless(app);
